d42d6d13563ab29a3b28f3a648af35f6f25b3d07
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2019-01-13 22:55:37 -0200

    criando os pacotes model e repository

---
+++ src/main/java/produtos/com/apirest/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package produtos.com.apirest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import produtos.com.apirest.models.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+
+}
