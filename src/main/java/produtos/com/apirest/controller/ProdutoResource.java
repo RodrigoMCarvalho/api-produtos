86677d8c90486fc6506a7b52fae19c6e79f39df9
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2019-01-15 21:20:55 -0200

    implementando os métodos DELETE e PUT

--- src/main/java/produtos/com/apirest/controller/ProdutoResource.java
+++ src/main/java/produtos/com/apirest/controller/ProdutoResource.java
@@ -1,15 +1,18 @@
 package produtos.com.apirest.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import produtos.com.apirest.error.ResourceNotFoundException;
 import produtos.com.apirest.models.Produto;
 import produtos.com.apirest.repository.ProdutoRepository;
 
@@ -21,18 +24,39 @@ public class ProdutoResource {
 	private ProdutoRepository repo;
 	
 	@GetMapping("/produtos")
-	public List<Produto> listProdutos(){
-		return repo.findAll();
+	public ResponseEntity<?> listProdutos(){
+		return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
 	}
 	
 	@GetMapping("/produto/{id}")
-	public Produto listProdutoPorId(@PathVariable(value="id") long id){
-		return repo.findById(id);
+	public ResponseEntity<?> listProdutoPorId(@PathVariable(value="id") long id){
+		verificaSeProdutoExiste(id);
+		return new ResponseEntity<>(repo.findById(id), HttpStatus.OK);
 	}
 	
 	@PostMapping("/produto")
-	public Produto save (@RequestBody Produto produto) {
-		return repo.save(produto);
+	public ResponseEntity<?> salvar(@RequestBody Produto produto) {
+		return new ResponseEntity<>(repo.save(produto), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/remover/{id}")
+	public ResponseEntity<?> remover(@PathVariable(value="id") long id) {
+		verificaSeProdutoExiste(id);
+		repo.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@PutMapping("/produto")
+	public ResponseEntity<?> editar(@RequestBody Produto produto){
+		verificaSeProdutoExiste(produto.getId());
+		return new ResponseEntity<>(repo.save(produto), HttpStatus.OK);
+	}
+	
+	public void verificaSeProdutoExiste(long id) {
+		Produto produto = repo.findById(id);
+		if(produto == null) {
+			throw new ResourceNotFoundException("Não foi encontrado um produto para o ID: " + id);
+		}
 	}
 	
 	
